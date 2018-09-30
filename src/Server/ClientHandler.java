e9feaeb3a982557c38dfc1ec1a44c10770532655
Mikael Larsson <larsson.mikael96@gmail.com>
2018-09-30 13:51:56 +0200

    Added Server and ClientHandler

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -1,4 +1,60 @@
 package Server;
 
-public class ClientHandler {
+import Server.Interfaces.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+    Socket socket;
+    ArrayList<Message> messages;
+    ObjectInputStream in;
+    ObjectOutputStream out;
+
+
+    public ClientHandler(Socket socket, ArrayList<Message> messages) {
+        this.socket = socket;
+        System.out.println("Client connected.");
+        this.messages = messages;
+
+        try {
+            this.in = new ObjectInputStream(socket.getInputStream());
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+
+        } catch(IOException e) {
+            System.err.println(String.format("Could not open streams for %s", socket.getRemoteSocketAddress()));
+        }
+    }
+
+    void close() {
+        try {
+            socket.close();
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            System.err.println(String.format("Could not close streams for %s, retrying...", socket.getRemoteSocketAddress()));
+            close();
+        }
+    }
+
+
+
+    @Override
+    public void run() {
+        while(socket.isConnected()) {
+            try {
+                Message message = (Message) in.readObject();
+                messages.add(message);
+
+            } catch (IOException e) {
+                System.err.println(String.format("Could not receive message from %s", socket.getRemoteSocketAddress()));
+            } catch (ClassNotFoundException e) {
+                System.err.println(String.format("Wrong object received from %s", socket.getRemoteSocketAddress()));
+            }
+        }
+    }
+
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,43 @@
 package Server;
 
+import Server.Interfaces.Message;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
 public class Server {
+    private ServerSocket serverSocket;
+    private boolean running = true;
+    private ArrayList<ClientHandler> clients = new ArrayList<>(); // TODO - implement client.
+    // TODO - add a message queue.
+
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println(String.format("Listening on port %s...", port));
+
+        } catch(IOException e) {
+            System.err.println(String.format("Port %s is already open.", port));
+        }
+    }
+
+    public void listen() {
+        // TODO - fix when client is implemented
+        try {
+            ClientHandler client = new ClientHandler(serverSocket.accept(), messages);
+            client.start(); // TODO implement client.
+            clients.add(client);
+
+        } catch(IOException e) {
+            System.err.println("Could not connect to client.");
+        }
+    }
+
+
+    public static void main(String args[]) throws Exception {
+        Server server = new Server(8080);
+        server.listen();
+
+    }
 }
