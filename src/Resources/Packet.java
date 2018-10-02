17e0b7f53b5f819db47736ba863e76e26969e113
Mikael Larsson <larsson.mikael96@gmail.com>
2018-10-02 11:11:15 +0200

    Changed implementation

--- src/Resources/Packet.java
+++ src/Resources/Packet.java
@@ -5,31 +5,32 @@ import java.io.Serializable;
 public class Packet implements Serializable {
     private String username;
     private String data;
+    public final static int PASSWORD = 1;
+    public final static int TEXT = 2;
 
     // used for determine if the data is for a password or a message.
-    private String flag; // Flag "p" = password
-                         // flag "t" = text
+    private int flag;
 
     /*
     Example 1 - Sending a login packet to the server.
-    Packet login = new Packet("p", "TestUser", "password123");
+    Packet login = new Packet(Packet.PASSWORD, "TestUser", "password123");
     clientOutputStream.writeObject(login);
 
     Example 2 - Sending a text to the chat when the user is logged in.
-    Packet message = new Packet("t", "TestUser", "Hello everyone!");
+    Packet message = new Packet(Packet.TEXT, "TestUser", "Hello everyone!");
     clientOutputStream.writeObject(login);
      */
 
 
     // use to send messages to the server
-    public Packet(String flag, String username, String data) {
+    public Packet(int flag, String username, String data) {
         this.flag = flag;
         this.username = username;
         this.data = data;
     }
 
 
-    public String getFlag() {
+    public int getFlag() {
         return flag;
     }
 
