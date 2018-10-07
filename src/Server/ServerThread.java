b7bdca8b3ddc55c628812695e3f35a55a24b5195
Mikael Larsson <larsson.mikael96@gmail.com>
2018-10-07 12:03:13 +0200

    Implemented ServerThread to handle outgoing messages

--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -1,4 +1,40 @@
 package Server;
 
-public class ServerThread {
+
+import Resources.Packet;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+// Used for sending messages from the message queue to clients
+public class ServerThread extends Thread {
+
+
+    private void send(ClientHandler client, Packet packet) {
+        try {
+            client.out.writeObject(packet);
+        } catch(IOException e) {
+            System.err.println("Could not send message to client.");
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            if(!Server.clients.isEmpty()) {
+                Packet currentPacket = Server.messageQueue.pop();
+
+                for (ClientHandler client : Server.clients) {
+                    send(client, currentPacket);
+                }
+            }
+        }
+
+
+    }
+
+
+
+
 }
