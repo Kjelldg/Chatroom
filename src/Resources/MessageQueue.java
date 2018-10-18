ef5f419fd72ab64aee86d83290adbc68d13ed3d4
Mikael Larsson <larsson.mikael96@gmail.com>
2018-10-18 22:34:55 +0200

    Added a size function for debugging.

--- src/Resources/MessageQueue.java
+++ src/Resources/MessageQueue.java
@@ -1,5 +1,6 @@
 package Resources;
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 public class MessageQueue {
     ArrayList<Packet> mq = new ArrayList<>();
@@ -7,15 +8,23 @@ public class MessageQueue {
 
     // push packet to array
     public void push(Packet p) {
+        System.out.println("Packet has been pushed to the queue");
         mq.add(p);
     }
 
     // gets and removes the first element of the array
     public Packet pop() {
-        Packet p = mq.get(0);
-        mq.remove(0);
-        return p;
+
+        if(!mq.isEmpty()) {
+            Packet p = mq.get(0);
+            mq.remove(0);
+            return p;
+        } else {
+            throw new EmptyStackException();
+        }
     }
 
-
+    public int size() {
+        return this.mq.size();
+    }
 }
