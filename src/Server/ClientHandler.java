19bc6fb82c3055c355e07d7630cc3a409247d077
kjelldg <Kjelldg@gmail.com>
2018-10-21 10:00:58 +0200

    Integrated writing messages and usernames to XML logic.

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -1,6 +1,7 @@
 package Server;
 
 import Resources.Packet;
+import xml_history.*;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -13,6 +14,7 @@ class ClientHandler extends Thread {
 	private String username;
 	public ObjectOutputStream out;
 	private boolean loggedIn = false;
+	private Write_XML write_XML = new Write_XML();
 
 	// In charge of creating a thread for the client and handling packets from the
 	// client.
@@ -53,16 +55,15 @@ class ClientHandler extends Thread {
 		// logs in the client if the user exist
 		if (initPacket.getFlag() == Packet.PASSWORD && Server.database.userExistence(username)) {
 			loggedIn = Server.database.logOnUser(username, password);
-			
-			if(loggedIn) {
+
+			if (loggedIn) {
 				this.username = username;
 				try {
 					out.writeObject(new Packet(Packet.TEXT, "Server", "Logged in"));
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			}
-			else {
+			} else {
 				try {
 					out.writeObject(new Packet(Packet.TEXT, "Server", "Wrong user credentials, could not log in"));
 				} catch (IOException e) {
@@ -70,7 +71,7 @@ class ClientHandler extends Thread {
 				}
 			}
 		}
-		//if Client has sent a Password packet and user doesn't exist, create one!
+		// if Client has sent a Password packet and user doesn't exist, create one!
 		else if (initPacket.getFlag() == Packet.PASSWORD && !Server.database.userExistence(username)) {
 			Server.database.createUser(username, password);
 			System.out.println(String.format("User %s has been created.", username));
@@ -108,14 +109,15 @@ class ClientHandler extends Thread {
 				if (userMessage.getFlag() == Packet.TEXT) {
 					// store message
 					Server.messageQueue.push(userMessage);
+					// Stores message in separate XML-file.
+					write_XML.write_Message(userMessage.toString(), username);
 
 				} else {
 					System.err.println("Wrong packet flag sent from client.");
 				}
-				
-				//for testing purposes
-				//out.writeObject(new Packet(Packet.TEXT, "Client thread", "HEJ"));
 
+				// for testing purposes
+				// out.writeObject(new Packet(Packet.TEXT, "Client thread", "HEJ"));
 
 			} catch (IOException e) {
 				System.err.println(String.format("Could not receive message from %s", socket.getRemoteSocketAddress()));
--- src/xml_history/Write_XML.java
+++ src/xml_history/Write_XML.java
@@ -1,22 +1,16 @@
 package xml_history;
 
 import java.io.File;
-import java.util.Scanner;
+
+import com.sun.xml.internal.ws.api.message.Packet;
 
 public class Write_XML {
 
-	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Enter username: ");
-		String userName = scanner.nextLine();
-
-		System.out.println("Enter message: ");
-		String user_Input_Message = scanner.nextLine();
+	public void write_Message(String user_Input_Message, String userName) {
 
 		File xmlFile = new File("./UserMessage.xml");
 		boolean xmlFileBooleanExists = xmlFile.exists();
+		// String user_Input_Message_String = user_Input_Message.toString();
 
 		/*
 		 * Check if the file exists or not. If it exists, append the new message. If it
